f82544410ac3f52b519fa7be13a472459fd6d05a
phenoptix <ben@phenoptix.com>
2014-05-07 22:28:40 +0100

    Commented and cleaned up
    Functionality much the same but cleaned up a little and commented so at
    least I can understand it come tomorrow.

--- MeArmIK/MeArmIK.ino
+++ MeArmIK/MeArmIK.ino
@@ -11,11 +11,12 @@
 #include <Servo.h>
 #include <Arduino.h>
 
+const float pi = 3.14159;
 
 /* Arm Measurements */
-#define bicep 81 // bicep length in mm
-#define forearm 81 // forearm length in mm
-#define base 60// base height in mm
+const int bicep = 81; // bicep length in mm
+const int forearm = 81; // forearm length in mm
+const int base = 60; // base height in mm
  
 Servo myservo0, myservo1, myservo2, myservo3;  // create servo object to control a servo 
  
@@ -52,37 +53,49 @@ void loop()
 { 
   /*Starting by reading x and y from potpins we'll use these to calculate the position of the servos*/
   
-  x = analogRead(potpin0);        // read potpin0 and set to x
-  x = map(x, 0, 1023, 20-base, 310-base);   // scale pot value between the min and max values of x on the arm
-  delay(15);   // reads the value of the potentiometer (value between 0 and 1023) 
-  y = analogRead(potpin3);            // reads the value of the potentiometer (value between 0 and 1023) 
-  y = map(y, 0, 1023, 24, 180);   // scale pot value between the min and max values of x on the arm
-  delay(15); 
+  x = analogRead(potpin0);                 // read potentiometer on A0 and set to x (value between 0 and 1023)
+  x = map(x, 0, 1023, 20-base, 300-base);  // scale pot value between the min and max values of x on the arm
+  delay(15);                               // give it a moment 
+  y = analogRead(potpin3);                 // read potentiometer on A3 and set to y (value between 0 and 1023) 
+  y = map(y, 0, 1023, 24, 180);            // scale pot value between the min and max values of x on the arm
+  delay(15);                               // give it a moment
    
- int b = sqrt ((x * x) + (y * y));
+ int b = sqrt ((x * x) + (y * y));        // b = distance from the origin to the start of the gripper
  //Serial.write(b);
- float q1 = atan2( x, y );
+ float q1 = atan2( x, y );                // q1 = angle between the horizontal and the line b
  //Serial.print(q1,4);
- float q2 = acos((bic_sq - for_sq + (b * b))/(2 * bicep * b));
+ float q2 = acos((bic_sq - for_sq + (b * b))/(2 * bicep * b)); // q1 = angle between line b and the bicep
  //Serial.print(q2,4);
- float abi = q1 + q2;
+ float abi = q1 + q2;                     // abi = angle between horizontal and the bicep
  //Serial.print(abi,4);
- float afo = acos((bic_sq + for_sq - (b * b))/(2 * bicep * forearm));
- Serial.print(abi*57.3,4);
- Serial.print(afo*57.3,4);
-  
-
-  myservo0.write(180-(ftl(abi*57.3)));                 // sets the servo position according to the scaled value 
-  myservo3.write(ftl(afo*57.3));                  // sets the servo position according to the scaled value 
-  delay(15);
-  
-  delay(15);                           // waits for the servo to get there
-  val1 = analogRead(potpin1);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val1 = map(val1, 0, 1023, 40, 90);     // scale it to use it with the gripper (40 and 90 degrees) 
-  myservo1.write(val1);   // sets the servo position according to the scaled value 
-  delay(15);                           // waits for the servo to get there 
-  val2 = analogRead(potpin2);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val2 = map(val2, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
-  myservo2.write(val2);                  // sets the servo position according to the scaled value 
-  delay(15);                           // waits for the servo to get there  
+ float afo = acos((bic_sq + for_sq - (b * b))/(2 * bicep * forearm)); // afo = angle between bicep and forearm
+ 
+ 
+ // Values passed to servo should be between 0 and 180 or more like 0 and 110 for the forearm servo
+ // and 0 and 160 for the bicep. Should look at how to set the servos and would be good to avoid the
+ // far extremes of the gear as in low quality servos this is where you get strain if the gears are
+ // incorrectly set. That's a physical issue but while I'm thinking I'll write it down!
+ 
+ // Also convert from Radians to degrees
+ //abi = (pi * abi) - abi; // angle calculated is the inverse of the servo 
+ //abi = map(abi, 0, pi, 180, 0); // this didn't work
+ //afo = map(afo, 0, pi, 0, 180); // nor did this!
+ 
+ abi = 180 - (abi * 57.3);
+ afo = (afo * 57.3);
+ 
+ Serial.println(abi);
+ Serial.println(afo);
+ 
+ myservo0.write(abi);           // sets the servo position according to the scaled value 
+ myservo3.write(afo);           // sets the servo position according to the scaled value 
+ delay(30);
+ val1 = analogRead(potpin1);            // reads the value of the potentiometer (value between 0 and 1023) 
+ val1 = map(val1, 0, 1023, 40, 90);     // scale it to use it with the gripper (40 and 90 degrees) 
+ myservo1.write(val1);   // sets the servo position according to the scaled value 
+ delay(15);                           // waits for the servo to get there 
+ val2 = analogRead(potpin2);            // reads the value of the potentiometer (value between 0 and 1023) 
+ val2 = map(val2, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
+ myservo2.write(val2);                  // sets the servo position according to the scaled value 
+ delay(15);                           // waits for the servo to get there  
 } 
